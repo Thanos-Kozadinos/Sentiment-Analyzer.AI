bc58c526a5af5ae450d3b5c419a27167ca07adf8
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-03-21 17:26:48 +0100

    moooooooore

--- mlFullStackHackDay.Api/Program.cs
+++ mlFullStackHackDay.Api/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.Extensions.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddPredictionEnginePool<SampleObservation, SamplePrediction>()
+                    .FromFile(builder.Configuration["MLModel:MLModelFilePath"]);
 // Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -26,11 +29,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 
-builder.Services.AddPredictionEnginePool<SampleObservation, SamplePrediction>()
-                    .FromFile(builder.Configuration["MLModel:MLModelFilePath"]);
+
 
 using (var scope = app.Services.CreateScope())
 {
