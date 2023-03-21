0013a04bd5e42f730f752ad7825cd181ea5f7b18
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-03-21 16:00:17 +0100

    more more azure test

--- mlFullStackHackDay.Api/Program.cs
+++ mlFullStackHackDay.Api/Program.cs
@@ -13,8 +13,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.")));
 
 
 builder.Services.AddPredictionEnginePool<SampleObservation, SamplePrediction>()
