bfd3d7fcb7a50f47e334fd166d6c35513fb8cac8
salt-dev <salt-dev@salt-devs-MacBook-Pro.local>
2023-03-25 22:49:53 +0100

    full stack deployed

--- mlFullStackHackDay.Frontend/src/Services/Services.ts
+++ mlFullStackHackDay.Frontend/src/Services/Services.ts
@@ -1,19 +1,23 @@
 import axios from 'axios';
 import {IFile, INewUser, ISentence, IUpdateUser, IUser,} from './Interfaces'
 
+// https://newsentimentaiapi.azurewebsites.net/
 
 export const getUsersDataAxios = async () => {
-  const response = await axios.get<IUser[]>('http://localhost:5290/api/GetUsers');
+  const response = await axios.get<IUser[]>('https://newsentimentaiapi.azurewebsites.net/api/GetUsers');
+  // const response = await axios.get<IUser[]>('http://localhost:5290/api/GetUsers');
   return response.data;
 };
 
 export const getDataFromFileAxios = async () => {
-  const response = await axios.get<IFile[]>('http://localhost:5290/api/GetDataFromFile');
+  // const response = await axios.get<IFile[]>('http://localhost:5290/api/GetDataFromFile');
+  const response = await axios.get<IFile[]>('https://newsentimentaiapi.azurewebsites.net/api/GetDataFromFile');
   return response.data;
 };
 
 export const addNewUser = async (user: INewUser) => {
-  const response = await fetch("http://localhost:5290/api/createUser", {
+  // const response = await fetch("http://localhost:5290/api/createUser", {
+    const response = await fetch("https://newsentimentaiapi.azurewebsites.net/api/createUser", {
     method: "POST",
     body: JSON.stringify(user),
     headers: {
@@ -26,7 +30,9 @@ export const addNewUser = async (user: INewUser) => {
 
 export const updateUser = async (user: IUpdateUser) => {
   // const url = `{http://localhost:5290/api/updateUser/${user.id}}`;
-  const url = "http://localhost:5290/api/updateUser";
+  // const url = "http://localhost:5290/api/updateUser";
+  const url = "https://newsentimentaiapi.azurewebsites.net/api/updateUser";
+
   console.log(url)
   const response = await fetch(url, {
     method: "PUT",
